976dcfe5702be6527f2b500e5c96c6e467d864ea
Pareek <pareeka2@diebold.com>
2018-01-02 12:30:47 +0530

    First Commit

---
+++ Semaphore/EventManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semaphore
+{
+    public delegate void MyDel();
+    class EventManager
+    {
+        public event MyDel ItemsRejected;
+        public event MyDel ItemsPresented;
+        public EventManager()
+        {
+            this.ItemsRejected += new MyDel(this.ItemsRejected_handler);
+            this.ItemsPresented += new MyDel(this.ItemsPresented_handler);
+        }
+
+        public void ItemsRejected_handler()
+        {
+            Console.WriteLine(">>> ItemsRejected");
+            Console.WriteLine("<<< ItemsRejected");
+        }
+
+        public void ItemsPresented_handler()
+        {
+            Console.WriteLine(">>> ItemsPresented");
+            Console.WriteLine("<<< ItemsPresented");
+        }
+    }
+}
---
+++ Semaphore/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SampleApp
+{
+    public delegate void MyDel();
+
+    class EventProgram
+    {
+        event MyDel ItemsRejected;
+        event MyDel ItemsPresented;
+        public EventProgram()
+        {
+            this.ItemsRejected += new MyDel(this.ItemsRejected_handler);
+            this.ItemsPresented += new MyDel(this.ItemsPresented_handler);
+        }
+
+        public void ItemsRejected_handler()
+        {
+            Console.WriteLine(">>> ItemsRejected");
+            Console.WriteLine("<<< ItemsRejected");
+        }
+
+        public void ItemsPresented_handler()
+        {
+            Console.WriteLine(">>> ItemsPresented");
+            Console.WriteLine(">>> ItemsPresented");
+        }
+        static void Main(string[] args)
+        {
+            EventProgram obj1 = new EventProgram();
+            obj1.ItemsPresented();
+            obj1.ItemsRejected();
+        }
+
+    }
+}
